ef0f7a7acea045309caae58512432eab3cbeba32
uttamtyagi.111 <uttamtyagi.111@gmail.com>
2025-09-13 13:28:01 +0530

    changed config

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://visitor-management-frontend-eight.vercel.app/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://visitor-management-system-vhga.onrender.com/api';
 const APP_TITLE = import.meta.env.VITE_APP_TITLE || 'Visitor Management System';
 
 export { API_BASE_URL, APP_TITLE };
