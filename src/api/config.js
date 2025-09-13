bfc2e287316540878f1a9b134b8b0cc60b4b70d6
uttamtyagi.111 <uttamtyagi.111@gmail.com>
2025-09-13 11:28:52 +0530

    base url changed

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://visitor-management-frontend-eight.vercel.app/login/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://visitor-management-frontend-eight.vercel.app/api';
 const APP_TITLE = import.meta.env.VITE_APP_TITLE || 'Visitor Management System';
 
 export { API_BASE_URL, APP_TITLE };
